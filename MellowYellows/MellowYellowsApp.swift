f646366b18f8cd4f8d6c29902c8fd06d9cd5cef9
snack <kwansiknam@gmail.com>
2023-09-27 17:17:31 +0900

    Initial Commit

---
+++ MellowYellows/MellowYellowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MellowYellowsApp.swift
+//  MellowYellows
+//
+//  Created by 관식 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct MellowYellowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
