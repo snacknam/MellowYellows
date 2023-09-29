5c927a76792e168717be35eefbb3a3725ecd430d
snack <kwansiknam@gmail.com>
2023-09-29 16:27:53 +0900

    feat: add MainView

--- MellowYellows/MellowYellowsApp.swift
+++ MellowYellows/MellowYellowsApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MellowYellowsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
+                .preferredColorScheme(.dark)
         }
     }
 }
---
+++ MellowYellows/View/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MellowYellows
+//
+//  Created by 관식 on 2023/09/27.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
