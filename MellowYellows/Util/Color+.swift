41d5088acae2c6444a3ff110a9de4217bc00e4bb
snack <kwansiknam@gmail.com>
2023-09-29 16:53:31 +0900

    feat: add assets and colors

---
+++ MellowYellows/Util/Color+.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+Color.swift
+//  MellowYellows
+//
+//  Created by 관식 on 2023/09/29.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("background")
+    static let subGray = Color("subGray")
+    static let accentYellow = Color("accentYellow")
+    static let lightYellow = Color("lightYellow")
+}
