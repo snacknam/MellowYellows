e50ec43037a427438b8a46b99f3fd103a174e2e1
snack <kwansiknam@gmail.com>
2023-10-09 19:45:08 +0900

    feat: add BodyView

--- MellowYellows/View/MainView.swift
+++ MellowYellows/View/MainView.swift
@@ -13,9 +13,10 @@ struct MainView: View {
             Color("background")
                 .ignoresSafeArea()
             StarView()
-            VStack {
+            MoonView()
+            VStack(spacing: 320) {
                 HeaderView()
-                MoonView()
+                BodyView()
             }
         }
     }
@@ -30,6 +31,17 @@ extension MainView {
                 .font(.system(size: 40, weight: .medium))
         }
         .foregroundColor(.white)
+        .frame(height: 160)
+    }
+    
+    private func BodyView() -> some View {
+        Text("The moon is almost gone, but the stars are still shining bright. Take a photo that showcases the contrast between the moon's fading light and the sparkling sky!")
+            .foregroundColor(.white)
+            .font(.system(size: 18, weight: .light))
+            .multilineTextAlignment(.center)
+            .frame(height: 140, alignment: .top)
+            .padding(.horizontal, 24)
+            .lineSpacing(4)
     }
 }
 
